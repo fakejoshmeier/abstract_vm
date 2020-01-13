2edc78496f4c32ea26b151707b8e27788747e891
Joshua Meier <joshua.meier2@gmail.com>
2020-01-13 15:30:22 -0800

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -18,35 +18,54 @@ int		error(std::string error, int flag) {
 	return (-1);
 }
 
+void	action(std::string line) {
+	std::string	cmd;
+	std::string	tp;
+	std::string	nbr;
+	IOperand	*op;
+
+	if (line == "" || line[0] == ';')
+		return ;
+	int i = 0;
+	while (line[i] && line[i] != ' ')
+		cmd += line[i++];
+	//Check hashmap for command
+	if (!hash[cmd])
+		throw();
+	//Command exists, run lexer
+	op = lexer();
+	hash[cmd](op);
+}
+
 int		main(int ac, char **av) {
 	std::string line;
+
 	int			lineNumber = 0;
 
-	if (ac > 2)
-		return (error(NULL, USAGE_ERROR));
+	if (ac > 2) return (error(NULL, USAGE_ERROR));
 	if (ac == 1) { // Begin reading from stdin, as no file was provided
 		std::ofstream file("tmp");  // Write to a tmp file, meaning that I read from a file either way and save on code
 		while (std::cin.good()) {
 			std::getline(std::cin, line); // Eyyy, GNL's triumphant return.
-			if (line.compare(";;"))
-				break ;
+			if (line.compare(";;")) break;
 			file << line << std::endl;
 		}
 		file.close();
 	}
-	std::fstream file;
-	file.open(ac == 1 ? "tmp" : av[1]);
-	if (file.fail())
-		return (error(av[1]), READ_ERROR);
-	while (file.good()) {
-		std::getline(file, line);
-		++lineNumber;
-		try {}
-		catch () {
 
+	std::fstream fin;
+
+	ifn.open(ac == 1 ? "tmp" : av[1]);
+	if (fin.fail()) return (error(av[1]), READ_ERROR);
+	while (fin.good()) {
+		std::getline(fin, line);
+		++lineNumber;
+		try { action(line); }
+		catch (avmExceptions & e) {
+			std::cout << e.what() << " : line " << lineNumber << " : " << line << std::endl;
 		}
 	}
-	file.close();
-	remove("tmp");
+	fin.close();
+	if (ac == 1) remove("tmp");
 	return (0);
 }
